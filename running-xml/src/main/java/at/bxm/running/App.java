43f158f6de4aea3ec40fa9b601563376bee6e9a3
Martin <martin@bxm.at>
2010-10-26 13:42:07 +0200

    init

---
+++ running-xml/src/main/java/at/bxm/running/App.java
@@ -0,0 +1,13 @@
+package at.bxm.running;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
