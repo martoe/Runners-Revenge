4341b96d2208eca432c4b66906c6d5d68fe63c4a
Martin <martin@bxm.at>
2010-11-12 21:45:24 +0100

    dummy RCP app (http://www.vogella.de/articles/EclipseRCP/article.html)

---
+++ running-gui/src/main/java/at/bxm/running/gui/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,20 @@
+package at.bxm.running.gui;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "at.bxm.running.gui.perspective"; //$NON-NLS-1$
+
+	@Override
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+
+	@Override
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+}
---
+++ running-gui/src/main/java/at/bxm/running/gui/Perspective.java
@@ -0,0 +1,10 @@
+package at.bxm.running.gui;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	@Override
+	public void createInitialLayout(IPageLayout layout) {}
+}
