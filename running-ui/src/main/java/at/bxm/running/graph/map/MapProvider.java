97a4b4970c6d26ba08c04202f9b8656e4c17cc8a
Martin <martin@bxm.at>
2010-11-25 18:46:54 +0100

    moved to running-maps

--- running-ui/src/main/java/at/bxm/running/graph/map/MapProvider.java
+++
@@ -1,9 +0,0 @@
-package at.bxm.running.graph.map;
-
-@Deprecated
-public interface MapProvider {
-
-	MapLayout<?> getLayout(double latNorth, double latSouth, double lonEast, double lonWest,
-					double resolution);
-
-}
